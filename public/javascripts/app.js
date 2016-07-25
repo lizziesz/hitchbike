fde28c885a72611cd7060b5760abb0620f97e4e1
Lizzie Szoke <lizzieszoke@Lizzies-MacBook-Air.local>
2016-07-25 11:02:55 -0600

    Added some views and routes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,8 +3,14 @@ var app = angular.module("hitchBikeApp", ['angularMoment', 'ngAnimate', 'ngRoute
 app.config(function($routeProvider){
   $routeProvider
     .when('/', {
-      templateUrl: 'index.html',
+      templateUrl: 'views/landing.html',
       controller: 'HitchBikeController'
+    })
+    .when('/bikes', {
+      templateUrl: 'views/bikes.html'
+    })
+    .when('/dashboard', {
+      templateUrl: 'views/dashboard.html'
     })
     .otherwise( { redirectTo: '/' } );
   });
