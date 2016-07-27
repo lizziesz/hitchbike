0d76c6244c29fa2738ce4fb684f646ed26de64f4
Lizzie Szoke <lizzieszoke@Lizzies-MacBook-Air.local>
2016-07-27 15:03:59 -0600

    Getting jwt on localStorage

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 var bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
-var secret="secretsareReal";
+var bearerToken = require('express-bearer-token');
 var token;
+
+function protect(req,res,next) {
+  console.log("REQ.JWT: " + req.token);
+  jwt.verify(req.token, process.env.SECRET, function (err,decoded) {
+    if (!err) {
+      next();
+    } else {
+      res.status(400).send('Bad Request');
+    }
+  });
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -83,7 +95,7 @@ router.post('/api/signin', function(req, res, next) {
     }
     else if(bcrypt.compareSync(req.body.password, data.password)) {
       console.log("Password correct");
-      token = jwt.sign({ id: data.id, username: data.username, is_admin: data.is_admin }, secret);
+      token = jwt.sign({ id: data.id, username: data.username, is_admin: data.is_admin }, process.env.SECRET);
       res.json({token:token});
       console.log("token token: " + token);
       // res.redirect('/bikes');
@@ -98,6 +110,7 @@ router.post('/api/signup', function(req, res, next) {
   console.log(req.body.zip_code);
   console.log(typeof(req.body.zip_code));
   var zip = parseInt(req.body.zip_code);
+  console.log(req.body.street_address);
   console.log("ZIPPY: " + zip);
   knex('users')
   .where({
@@ -120,16 +133,36 @@ router.post('/api/signup', function(req, res, next) {
         is_admin: false
       }).returning("*")
       .then(function(user) {
-        var listedItems = {id: user[0].id, username: user[0].username, is_admin: user[0].is_admin};
-        token = jwt.sign({ id: user[0].id}, secret);
-        res.json({token:token, user:listedItems});
+        token = jwt.sign({ id: user[0].id, username: user[0].username, is_admin: user[0].is_admin}, process.env.SECRET);
+        console.log(token);
+        res.json({token:token});
         // res.redirect('/bikes');
       }).catch(function(err) {
         console.log(err);
       })
     }
   })
-})
+});
+
+router.post('/api/addbike', protect,function(req, res, next) {
+  knex('bikes').insert({
+    // owner_id:
+    title: req.body.title,
+    description: req.body.description,
+    picture: req.body.picture,
+    type: req.body.type,
+    condition: req.body.condition,
+    price_day: req.body.price_day,
+    price_hour: req.body.price_hour,
+    instructions: req.body.instructions,
+    street_address: req.body.street_address,
+    city: req.body.city,
+    state: req.body.state,
+    zip_code: req.body.zip_code
+  }).then(function(data){
+    res.redirect('/#/');
+  });
+});
 
 
 
