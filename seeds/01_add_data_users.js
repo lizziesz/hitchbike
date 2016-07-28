104916d07bef67178925ae739a8eff235a0f2aa2
Jeff <medders.jeffrey@gmail.com>
2016-07-28 10:07:33 -0600

    toLowerCase database functionality, changed some seeds, fixed sign up error with street address

--- seeds/01_add_data_users.js
+++ seeds/01_add_data_users.js
@@ -6,57 +6,52 @@ exports.seed = function(knex, Promise) {
     knex('users').del(),
     // Inserts seed entries
     knex('users').insert({
-        id: 1,
         username: 'andrew',
         password: bcrypt.hashSync('test', 8),
         email: "westaa@colorado.edu",
         street_address: '220 Brightwood St.',
-        city: "San Antonio",
-        state: "TX",
+        city: "san antonio",
+        state: "tx",
         zip_code: '78209',
         is_admin: true
     }),
     knex('users').insert({
-        id: 2,
         username: 'jeffrey',
         password: bcrypt.hashSync('test', 8),
         email: "medders.jeffrey@gmail.com",
         street_address: '2500 Pine St.',
-        city: "Boulder",
-        state: "CO",
+        city: "boulder",
+        state: "co",
         zip_code: '80302',
         is_admin: true
     }),
     knex('users').insert({
-        id: 3,
         username: 'paul',
         password: bcrypt.hashSync('password', 8),
         email: "made.up@gmail.com",
         street_address: '4500 North St.',
-        city: "Philadelphia",
-        state: "PA",
+        city: "philadelphia",
+        state: "pa",
         zip_code: '19019',
         is_admin: true
     }),
     knex('users').insert({
-        id: 4,
         username: 'lizzie',
         password: bcrypt.hashSync('Lizzie', 8),
         email: "dont.know@gmail.com",
         street_address: '405 Robinhood St.',
-        city: "Lafayette",
-        state: "CO",
+        city: "lafayette",
+        state: "co",
         zip_code: '80026',
         is_admin: true
     }),
     knex('users').insert({
-        id: 5,
         username: 'casey',
         password: bcrypt.hashSync('Casey', 8),
         email: "caseyisnotcool@gmail.com",
         street_address: '609 Beer St.',
-        city: "Milwaukee",
-        state: "WI",
+        city: "milwaukee",
+        state: "wi",
         zip_code: '53202',
         is_admin: true
     })
